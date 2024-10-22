6b4b68997afe1b3bd4b3a58e90dd267bc82cc3b5
p2110156 <quentin.bastos@etu.univ-lyon1.fr>
2024-10-22 14:45:33 +0200

    fix(migration):correct drop user id migration

--- migrations/Version20241022122119.php
+++ migrations/Version20241022122119.php
@@ -20,7 +20,6 @@ final class Version20241022122119 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE page DROP user_id');
         $this->addSql('ALTER TABLE target ADD page_id INT NOT NULL');
         $this->addSql('ALTER TABLE target ADD CONSTRAINT FK_466F2FFCC4663E4 FOREIGN KEY (page_id) REFERENCES page (id)');
         $this->addSql('CREATE INDEX IDX_466F2FFCC4663E4 ON target (page_id)');
@@ -29,7 +28,6 @@ final class Version20241022122119 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE page ADD user_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE target DROP FOREIGN KEY FK_466F2FFCC4663E4');
         $this->addSql('DROP INDEX IDX_466F2FFCC4663E4 ON target');
         $this->addSql('ALTER TABLE target DROP page_id');
