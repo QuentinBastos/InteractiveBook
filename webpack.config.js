b963b1101b8db7e8c4256b4dcb0e2b6c168407fb
nathan <nathan.garcia@etu.univ-lyon1.fr>
2024-11-13 11:47:34 +0100

    [fix]: webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const Encore = require('@symfony/webpack-encore');
-const path = require('path');
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
@@ -7,18 +6,26 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 
 Encore
     .setOutputPath('public/build/')
+
     .setPublicPath('/build')
+
     .addEntry('app_js', './assets/app.js')
     .addStyleEntry('home_css', './assets/styles/home.scss')
     .enableStimulusBridge('./assets/controllers.json')
+
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
     .enableSassLoader()
     .enablePostCssLoader()
+
     .cleanupOutputBeforeBuild()
+
     .enableBuildNotifications()
+
     .enableSourceMaps(!Encore.isProduction())
+
     .enableVersioning(Encore.isProduction())
+
     .configureBabel((config) => {
         config.plugins.push('@babel/plugin-transform-class-properties');
     })
@@ -26,46 +33,10 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
+
     .copyFiles({
         from: './assets/img',
-        to: 'img/[path][name].[hash:8].[ext]'
+        to: 'img/[path][name].[hash:8].[ext]',
     });
 
-module.exports = {
-    mode: Encore.isProduction() ? 'production' : 'development',
-    entry: './assets/app.js',
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'public/build'),
-        publicPath: '/build/',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[ext]',
-                            outputPath: 'images/',
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        watchContentBase: true,
-        hot: true,
-    },
-};
+module.exports = Encore.getWebpackConfig();
